ba281370422ee67f3a38be6939fb9a36e1aba286
Chandan-Alwala <38454265+Chandan-Alwala@users.noreply.github.com>
2024-08-01 17:24:53 -0500

    Update Monthly Transactions 1.sql

--- Medium/Monthly Transactions 1.sql
+++ Medium/Monthly Transactions 1.sql
@@ -38,6 +38,18 @@
 -- | 2019-01  | DE      | 1           | 1              | 2000               | 2000                  |
 -- +----------+---------+-------------+----------------+--------------------+-----------------------+
 
+-- Solution2 
+select substring(trans_date,1,7) as month, 
+country, 
+count(id) as trans_count, 
+sum(case when state='approved' then 1 else 0 end) as approved_count, 
+sum(amount) as trans_total_amount, 
+sum(case when state='approved' then amount else 0 end) as approved_total_amount
+from transactions t 
+group by substring(trans_date,1,7), country 
+
+
+
 -- Solution
 with t1 as(
 select DATE_FORMAT(trans_date,'%Y-%m') as month, country, count(state) as trans_count, sum(amount) as trans_total_amount
